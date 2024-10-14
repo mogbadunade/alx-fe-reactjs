fed3e6e60ad07440c0595cc467d541c3a7f0ed39
mogbadunademma <mogbadunademma@gmail.com>
2024-10-14 12:11:35 +0100

    add these three components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from '.Header/'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
+ 
 
   return (
     <>
